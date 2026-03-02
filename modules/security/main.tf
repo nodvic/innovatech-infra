c743029e5a9106ac61304e1b8cc22e707f8c149b
nodvic <victor.mstrd@gmail.com>
2026-03-02 15:47:09 +0100

    Security module toegevoegd met regels voor SSH en HTTP

---
+++ modules/security/main.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "common" {
+  name        = "${var.vpc_name}-common-sg"
+  description = "Basis beveiliging voor ${var.vpc_name}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-common-sg"
+  }
+}
