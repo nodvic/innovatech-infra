0ee68d1dfac9aa8bafd4d6330a097acebf5eb466
nodvic <victor.mstrd@gmail.com>
2026-03-14 15:04:40 +0100

    fix van public route table

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -15,9 +15,9 @@ output "nat_gateway_id" {
 }
 
 output "public_route_table_id" {
-  value = aws_route_table.public[*].id
+  value = aws_route_table.public.id
 }
 
 output "private_route_table_id" {
-  value = aws_route_table.private.id
+  value = aws_route_table.private[*].id
 }
